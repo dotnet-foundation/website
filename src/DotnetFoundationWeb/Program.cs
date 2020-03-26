560245b6413a6f9674c11139908b04f3af95563a
Jamie Wilbraham <jamie@zarmada.com>
2020-03-26 15:17:57 +1300

    init

---
+++ src/DotnetFoundationWeb/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Markdown;
+using Statiq.Web;
+
+namespace TestBlog
+{
+    public static class Program
+    {
+          public static async Task<int> Main(string[] args) =>
+              await Bootstrapper.Factory
+                  .CreateWeb(args)
+                  .BuildPipeline(
+                      "input",
+                      builder => builder
+                          .WithInputReadFiles("**/*.md")
+                          .WithProcessModules(new RenderMarkdown())
+                          .WithOutputWriteFiles(".html"))
+                  .RunAsync();
+    }
+}
