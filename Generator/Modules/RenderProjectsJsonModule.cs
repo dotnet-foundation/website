729f527506a4fbf32867f591df21d412d10719a3
Claire Novotny <claire@novotny.org>
2020-05-01 09:44:51 -0400

    restructure

---
+++ Generator/Modules/RenderProjectsJsonModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DotnetFoundationWeb.Models;
+using DotnetFoundationWeb.Utils;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+
+namespace DotnetFoundationWeb.Modules
+{
+  public class RenderProjectsJsonModule : Module
+  {
+    private JsonSerializerOptions _options;
+
+    public RenderProjectsJsonModule()
+    {
+    }
+
+    public RenderProjectsJsonModule WithOptions(JsonSerializerOptions options)
+    {
+      _options = options ?? throw new ArgumentNullException(nameof(options));
+      return this;
+    }
+
+    public RenderProjectsJsonModule WithOptions(Action<JsonSerializerOptions> modifyOptions)
+    {
+      modifyOptions?.Invoke(_options);
+      return this;
+    }
+
+    protected override async Task<IEnumerable<IDocument>> ExecuteContextAsync(IExecutionContext context)
+    {
+      context.LogDebug("Processing RenderProjectsJsonModule for ", context.Inputs.Length, " files");
+
+      var projects = new List<Project>();
+      foreach (IDocument input in context.Inputs)
+      {
+        await AddJsonAsync(input);
+      }
+      var json = JsonSerializer.Serialize(projects);
+      return context.CreateDocument(await context.GetContentProviderAsync(json, MediaTypes.Json)).Yield();
+
+      async Task AddJsonAsync(IDocument input)
+      {
+        var content = await input.GetContentStringAsync();
+        projects.Add(new Project
+        {
+          Title = StringUtils.GetDocValue("Title", content),
+          Logo = StringUtils.GetDocValue("Logo", content, "logo_big.png"),
+          Web = StringUtils.GetDocValue("Web", content, "#"),
+          Keywords = StringUtils.GetDocValue("Keywords", content),
+          Content = StringUtils.GetDocContent(content)
+        });
+      }
+    }
+  }
+}
---
+++ Generator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetFoundationWeb.Modules;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Web;
+
+namespace DotnetFoundationWeb
+{
+
+  public static class Program
+  {
+    public static async Task<int> Main(string[] args)
+    {
+      Func<string, string> formatter = null;
+      if (!string.IsNullOrWhiteSpace(AppSettings.ServerUri))
+      {
+        var formatterString = AppSettings.ServerUri + "/{0}";
+        formatter = f => string.Format(formatterString, f);
+      }
+
+      return await Bootstrapper.Factory
+        .CreateWeb(args)
+        .AddSetting(
+          Keys.DestinationPath,
+          Config.FromDocument(
+              doc => GetPath(doc)))
+        .AddSetting(
+          Keys.SitemapItem,
+          Config.FromDocument(
+              doc =>
+              {
+                var siteMapItem = new SitemapItem(GetPath(doc).FullPath);
+                return siteMapItem;
+              }))
+        .BuildPipeline(
+          "projects-json-generation",
+          builder => builder
+            .WithInputReadFiles("__data/projects/*.md")
+            .WithProcessModules(new RenderProjectsJsonModule())
+            .WithOutputWriteFiles(Path.Combine("projects", "projects.json"))
+        )
+        .BuildPipeline(
+          "generate-sitemap",
+          builder => builder
+            .WithInputReadFiles("**/*")
+            .WithInputModules(new SitemapGeneratorModule(formatter))
+            .WithOutputWriteFiles("sitemap.xml")
+        )
+        .BuildPipeline(
+          "rss-generation",
+          builder => builder
+            .WithInputReadFiles("__data/posts/*.md")
+            .WithOutputModules(new RssXmlGeneratorModule())
+            .WithOutputWriteFiles(Path.Combine("api", "rss", "index.xml"))
+        )
+        .RunAsync();
+    }
+
+    static FilePath GetPath(IDocument doc)
+    {
+      if (doc.Source.Directory.Segments.Last().SequenceEqual("posts".AsMemory()))
+      {
+        return new DirectoryPath("blog")
+          .Combine(new DirectoryPath(doc.Get<DateTime>("Published").ToString("yyyy/MM/dd")))
+          .CombineFile(doc.Destination.FileName.ChangeExtension(".html"));
+      }
+      else if (doc.Source.Directory.Segments.Last().SequenceEqual("campaign-2019".AsMemory()))
+      {
+        return new DirectoryPath("about/election/campaign-2019")
+          .CombineFile(doc.Destination.FileName.ChangeExtension(".html"));
+      }
+      else if (doc.Source.Directory.Segments.Last().SequenceEqual("projects".AsMemory()))
+      {
+        return new DirectoryPath("projects")
+          .CombineFile(doc.Destination.FileName.ChangeExtension(".html"));
+      }
+      else if (doc.Source.Directory.Segments.Last().SequenceEqual("committees".AsMemory()))
+      {
+        return new DirectoryPath("community/committees")
+          .CombineFile(doc.Destination.FileName.ChangeExtension(".html"));
+      }
+      else
+      {
+        return doc.Destination.ChangeExtension(".html");
+      }
+    }
+  }
+}
