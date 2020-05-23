bab6953d7866def9dc79567fea022478311e0603
Arhell <arhell333@gmail.com>
2020-05-23 14:56:57 +0300

    add meta tag "description"

--- AppSettings.cs
+++ AppSettings.cs
@@ -10,6 +10,8 @@ namespace DotnetFoundationWeb
     public static string ContactEmail { get; set; } = ConfigurationManager.AppSettings["ContactEmail"];
 
     public static string Title { get; set; } = ConfigurationManager.AppSettings["SiteTitle"];
+    
+    public static string Description { get; set; } = ConfigurationManager.AppSettings["SiteDescription"];
 
     public static string BaseRef { get; set; } = ConfigurationManager.AppSettings["BaseRef"];
 
