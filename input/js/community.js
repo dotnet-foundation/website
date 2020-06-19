d8d756bfebcce614962f5d77efde273c99a527a5
isaacrlevin <isaac.r.levin@gmail.com>
2020-06-19 14:02:04 -0700

    css updates

--- input/js/community.js
+++ input/js/community.js
@@ -8,14 +8,14 @@
       html += `<div class="card-columns">`;
 
       repos.forEach((repo) => {
-        html += `  <div class="card bg-light">
+        html += `  <div class="card bg-foundation">
                       <h3>${repo.repoName}</h3>
                       <div class="card-body text-center">`;
 
         html += ``;
         repo.issues.forEach((issue) => {
           html += `<p class="card-text">
-                      <a style="color: #68217a;" href="${issue.issueUrl}" target="_blank">${issue.issueTitle}</a>
+                      <a style="color: white" href="${issue.issueUrl}" target="_blank">${issue.issueTitle}</a>
                    </p>
                    <br />`;
         });
