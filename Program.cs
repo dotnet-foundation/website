fcf6b52a07b3e213f80fe1f91915cb8f19565f1b
Dave Glick <dave@daveaglick.com>
2020-09-25 11:27:19 -0400

    Speaker image is now calculated from the GitHub link if not provided

--- Program.cs
+++ Program.cs
@@ -23,36 +23,44 @@ namespace DotnetFoundationWeb
             }
 
             return await Bootstrapper.Factory
-              .CreateWeb(args)
-              .AddSetting(WebKeys.InputFiles, new [] { "**/{!_,}*", "_redirects" })
-              .AddSetting(Keys.Host, "dotnetfoundation.org")
-              .AddSetting(Keys.LinksUseHttps, true)
-              .AddSetting(WebKeys.MirrorResources, true)
-              .AddSetting(WebKeys.ExcludedPaths, "api/node_modules")
-              .BuildPipeline(
-                  "GenerateProjectsJson",
-                  builder => builder
-                      .WithDependencies(nameof(Statiq.Web.Pipelines.Content))
-                      .WithProcessModules(
-                          new ReplaceDocuments(nameof(Statiq.Web.Pipelines.Content)),
-                          new FlattenTree(),
-                          new FilterSources("projects/data/*.md"),
-                          new ExecuteConfig(Config.FromContext(ctx =>
-                              JsonSerializer.Serialize(ctx.Inputs.Select(x => new
-                              {
-                                  Title = x.GetString(WebKeys.Title),
-                                  Keywords = x.GetString(SiteKeys.Keywords),
-                                  Link = x.GetLink(),
-                                  Content = x.GetContentStringAsync().GetAwaiter().GetResult()
-                              })
-                          )))
-                      )
-                      .WithOutputWriteFiles("projects/projects.json")
-              )
-              .ModifyPipeline(
-                  nameof(Statiq.Web.Pipelines.Content),
-                  x => x.ProcessModules.Add(new GeocodeLocations(Config.FromSetting(SiteKeys.AzureMapsSubscriptionKey))))                
-              .RunAsync();
+                .CreateWeb(args)
+                .AddSetting(WebKeys.InputFiles, new [] { "**/{!_,}*", "_redirects" })
+                .AddSetting(Keys.Host, "dotnetfoundation.org")
+                .AddSetting(Keys.LinksUseHttps, true)
+                .AddSetting(WebKeys.MirrorResources, true)
+                .AddSetting(WebKeys.ExcludedPaths, "api/node_modules")
+                .BuildPipeline(
+                    "GenerateProjectsJson",
+                    builder => builder
+                        .WithDependencies(nameof(Statiq.Web.Pipelines.Content))
+                        .WithProcessModules(
+                            new ReplaceDocuments(nameof(Statiq.Web.Pipelines.Content)),
+                            new FlattenTree(),
+                            new FilterSources("projects/data/*.md"),
+                            new ExecuteConfig(Config.FromContext(ctx =>
+                                JsonSerializer.Serialize(ctx.Inputs.Select(x => new
+                                {
+                                    Title = x.GetString(WebKeys.Title),
+                                    Keywords = x.GetString(SiteKeys.Keywords),
+                                    Link = x.GetLink(),
+                                    Content = x.GetContentStringAsync().GetAwaiter().GetResult()
+                                })
+                            )))
+                        )
+                        .WithOutputWriteFiles("projects/projects.json")
+                )
+                .ModifyPipeline(
+                    nameof(Statiq.Web.Pipelines.Content),
+                    x => x.ProcessModules.Add(
+                        // Modules for speakers
+                        new ExecuteSources("community/speakers/*.md")
+                        {
+                            // TODO: URL validation
+                            new GeocodeLocations(Config.FromSetting(SiteKeys.AzureMapsSubscriptionKey)),
+                            new SpeakerImage()
+                        }
+                    ))
+                .RunAsync();
         }
     }
 }
