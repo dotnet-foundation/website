7311b53c4df56c2379ca0093ecff207ade565f91
Shawn Wildermuth <shawn@wildermuth.com>
2020-10-12 19:39:46 -0400

    Added to Speaker Blog to deal with redirects.

--- GetBlogFeeds.cs
+++ GetBlogFeeds.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.SyndicationFeed;
 using Microsoft.SyndicationFeed.Atom;
 using Microsoft.SyndicationFeed.Rss;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
@@ -40,95 +42,116 @@ namespace DotnetFoundationWeb
                     List<ISyndicationItem> items = new List<ISyndicationItem>();
                     using (HttpClient httpClient = context.CreateHttpClient())
                     {
+
                         httpClient.DefaultRequestHeaders.Add("User-Agent", "Wyam");
 
-                        using (Stream stream = await httpClient.GetStreamAsync(feed))
+                        var response = await httpClient.GetAsync(feed);
+                        if (response.StatusCode == HttpStatusCode.Redirect ||
+                            response.StatusCode == HttpStatusCode.MovedPermanently)
                         {
+                            context.LogWarning($"Attempting to follow redirect for {feed}");
+                            feed = response.Headers.Location.OriginalString;
+                            response = await httpClient.GetAsync(feed);
+                        }
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            using (Stream stream = await response.Content.ReadAsStreamAsync())
                             using (StreamReader streamReader = new XmlStreamReader(stream))
+                            using (XmlReader xmlReader = XmlReader.Create(streamReader, new XmlReaderSettings { Async = true, DtdProcessing = DtdProcessing.Ignore }))
                             {
-                                using (XmlReader xmlReader = XmlReader.Create(streamReader, new XmlReaderSettings { Async = true, DtdProcessing = DtdProcessing.Ignore }))
+                                xmlReader.MoveToContent();
+                                bool atom = xmlReader.Name == "feed";
+                                context.LogInformation($"Reading {feed} as " + (atom ? "Atom" : "RSS"));
+                                XmlFeedReader feedReader = atom
+                                    ? (XmlFeedReader)new AtomFeedReader(xmlReader, new FixedAtomParser())
+                                    : new RssFeedReader(xmlReader);
+                                while (await feedReader.Read())
                                 {
-                                    xmlReader.MoveToContent();
-                                    bool atom = xmlReader.Name == "feed";
-                                    context.LogInformation($"Reading {feed} as " + (atom ? "Atom" : "RSS"));
-                                    XmlFeedReader feedReader = atom
-                                        ? (XmlFeedReader)new AtomFeedReader(xmlReader, new FixedAtomParser())
-                                        : new RssFeedReader(xmlReader);
-                                    while (await feedReader.Read())
+                                    try
                                     {
-                                        try
+                                        switch (feedReader.ElementType)
                                         {
-                                            switch (feedReader.ElementType)
-                                            {
-                                                case SyndicationElementType.Person:
-                                                    ISyndicationPerson person = await feedReader.ReadPerson();
-                                                    if (person.RelationshipType == "author")
-                                                    {
-                                                        author = person.Name ?? person.Email;
-                                                    }
-                                                    break;
+                                            case SyndicationElementType.Person:
+                                                ISyndicationPerson person = await feedReader.ReadPerson();
+                                                if (person.RelationshipType == "author")
+                                                {
+                                                    author = person.Name ?? person.Email;
+                                                }
+                                                break;
 
-                                                case SyndicationElementType.Image:
-                                                    ISyndicationImage img = await feedReader.ReadImage();
-                                                    image = img.Url.ToString();
-                                                    break;
+                                            case SyndicationElementType.Image:
+                                                ISyndicationImage img = await feedReader.ReadImage();
+                                                image = img.Url.ToString();
+                                                break;
 
-                                                case SyndicationElementType.Link:
-                                                    ISyndicationLink link = await feedReader.ReadLink();
-                                                    website = link.Uri;
-                                                    break;
+                                            case SyndicationElementType.Link:
+                                                ISyndicationLink link = await feedReader.ReadLink();
+                                                website = link.Uri;
+                                                break;
 
-                                                case SyndicationElementType.Item:
-                                                    ISyndicationItem item = await feedReader.ReadItem();
-                                                    items.Add(item);
-                                                    break;
+                                            case SyndicationElementType.Item:
+                                                ISyndicationItem item = await feedReader.ReadItem();
+                                                items.Add(item);
+                                                break;
 
-                                                case SyndicationElementType.None:
-                                                    break;
+                                            case SyndicationElementType.None:
+                                                break;
 
-                                                default:
-                                                    ISyndicationContent content = await feedReader.ReadContent();
-                                                    if (string.Equals(content.Name, "title", StringComparison.OrdinalIgnoreCase))
-                                                    {
-                                                        title = content.Value;
-                                                    }
-                                                    else if (string.Equals(content.Name, "description", StringComparison.OrdinalIgnoreCase)
-                                                        || string.Equals(content.Name, "subtitle", StringComparison.OrdinalIgnoreCase))
-                                                    {
-                                                        description = content.Value;
-                                                    }
-                                                    break;
-                                            }
+                                            default:
+                                                ISyndicationContent content = await feedReader.ReadContent();
+                                                if (string.Equals(content.Name, "title", StringComparison.OrdinalIgnoreCase))
+                                                {
+                                                    title = content.Value;
+                                                }
+                                                else if (string.Equals(content.Name, "description", StringComparison.OrdinalIgnoreCase)
+                                                    || string.Equals(content.Name, "subtitle", StringComparison.OrdinalIgnoreCase))
+                                                {
+                                                    description = content.Value;
+                                                }
+                                                break;
                                         }
-                                        catch (Exception ex)
-                                        {
-                                            context.LogWarning($"Exception while processing {feedReader.ElementType} in {feed}: {ex.Message}");
-                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        context.LogWarning($"Exception while processing {feedReader.ElementType} in {feed}: {ex.Message}");
                                     }
                                 }
                             }
                         }
-                    }
 
-                    // Get a new document with feed metadata
-                    MetadataItems metadata = new MetadataItems();
-                    if (items.Count > 0)
-                    {
-                        BlogFeedItem[] feedItems = items
-                            .Select(x => new BlogFeedItem(x, _recent, website))
-                            .OrderByDescending(x => x.Published)
-                            .Take(50) // Only take the 50 most recent items
-                            .ToArray();
-                        metadata.Add(SiteKeys.BlogFeedItems, feedItems);
-                        return input.Clone(metadata).Yield();
+                        // Get a new document with feed metadata
+                        MetadataItems metadata = new MetadataItems();
+                        if (items.Count > 0)
+                        {
+                            BlogFeedItem[] feedItems = items
+                                .Select(x => new BlogFeedItem(x, _recent, website))
+                                .OrderByDescending(x => x.Published)
+                                .Take(50) // Only take the 50 most recent items
+                                .ToArray();
+                            metadata.Add(SiteKeys.BlogFeedItems, feedItems);
+                            return input.Clone(metadata).Yield();
+                        }
                     }
                 }
                 catch (Exception ex)
                 {
                     context.LogWarning($"Error getting feed for {feed}: {ex.Message}");
                 }
+            } 
+            else 
+            {
+              var speaker = input.GetString("Title");
+              if (!string.IsNullOrWhiteSpace(speaker))
+              {
+                context.LogWarning($"No RSS specified for {speaker}.");
+              }
+              else
+              {
+                context.LogWarning($"No RSS specified for unknown speaker.");
+              }
             }
-
+            
             return null;
         }
     }
