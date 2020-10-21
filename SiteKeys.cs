006b7c8bae60a8c2be5194a08e7575f3c23694c6
Sean Killeen <SeanKilleen@gmail.com>
2020-10-21 11:04:22 -0400

    Update linkexin regex

--- SiteKeys.cs
+++ SiteKeys.cs
@@ -27,7 +27,7 @@
         [SpeakerLink("fab fa-stack-overflow", true, true, "https://stackoverflow.com/users/[yourUserId]/[yourUsername]", "[Hh][Tt][Tt][Pp][Ss]:\\/\\/[Ss][Tt][Aa][Cc][Kk][Oo][Vv][Ee][Rr][Ff][Ll][Oo][Ww]\\.[Cc][Oo][Mm]\\/[Uu][Ss][Ee][Rr][Ss]\\/\\d+\\/.*")]
         public const string StackOverflow = nameof(StackOverflow);
         // Regex pattern test: https://regex101.com/r/IlrLEM/1
-        [SpeakerLink("fab fa-linkedin", true, true, "https://linkedin.com/in/[yourUsername]", "[Hh][Tt][Tt][Pp][Ss]:\\/\\/[Ll][Ii][Nn][Kk][Ee][Dd][Ii][Nn]\\.[Cc][Oo][Mm]\\/[Ii][Nn]\\/.*")]
+        [SpeakerLink("fab fa-linkedin", true, true, "https://linkedin.com/in/[yourUsername]", "[Hh][Tt][Tt][Pp][Ss]:\\/\\/([Ww][Ww][Ww].)?[Ll][Ii][Nn][Kk][Ee][Dd][Ii][Nn]\\.[Cc][Oo][Mm]\\/[Ii][Nn]\\/.*")]
         public const string LinkedIn = nameof(LinkedIn);
         // Regex pattern test: https://regex101.com/r/D1Kxdu/1
         [SpeakerLink("fab fa-twitch", true, true, "https://twitch.tv/[yourUsername]", "[Hh][Tt][Tt][Pp][Ss]:\\/\\/[Tt][Ww][Ii][Tt][Cc][Hh]\\.[Tt][Vv]\\/.*")]
