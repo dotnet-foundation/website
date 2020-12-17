28ea03ac47d54f6046a795e248433945c028d8f6
Dave Glick <dave@daveaglick.com>
2020-12-17 09:18:31 -0500

    Adds support for preferred pronouns to the speaker directory

---
+++ Analyzers/ValidateSpeakerPronouns.cs
@@ -0,0 +1,17 @@
+﻿using Statiq.Common;
+using System.Linq;
+
+namespace DotnetFoundationWeb
+{
+    public class ValidateSpeakerPronouns : SpeakerDataAnalyzer
+    {
+        protected override void AnalyzeSpeakerData(IDocument document, IAnalyzerContext context)
+        {
+            string pronouns = document.GetString(SiteKeys.Pronouns);
+            if (!pronouns.IsNullOrEmpty() && (pronouns.Count(x => x == '/') != 1 || pronouns.Any(x => x != '/' && (!char.IsLetter(x) || !char.IsLower(x)))))
+            {
+                context.AddAnalyzerResult(document, $"Preferred gender pronouns should be of the form [subject]/[object] in lowercase (I.e. \"they/them\")");
+            }
+        }
+    }
+}
